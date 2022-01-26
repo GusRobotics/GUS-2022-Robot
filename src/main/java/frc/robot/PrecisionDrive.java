69c549e4cd8467fc16ae6cd7bb94296cb8afb647
Brennan Lagasse <51427169+BrennanLagasse@users.noreply.github.com>
2022-01-25 20:09:19 -0500

    cleaner code in progress

--- src/main/java/frc/robot/PrecisionDrive.java
+++ src/main/java/frc/robot/PrecisionDrive.java
@@ -2,20 +2,42 @@ package frc.robot;
 
 // Neo Resources
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.SparkMaxPIDController;
 
 public class PrecisionDrive {
     private CANSparkMax m_left_drive;
     private CANSparkMax m_right_drive;
+    private SparkMaxPIDController m_pid_controller;
+    private double set_point;
+
+    // PID constants
+    public double kP, kI, kD, kIz, kFF, kMaxOutput, kMinOutput, maxRPM, maxVel, minVel, maxAcc, allowedErr;
 
     public PrecisionDrive(CANSparkMax m_left, CANSparkMax m_right) {
+        // Store reference to motors
         m_left_drive = m_left;
         m_right_drive = m_right;
+
+        // Set all of the parameters for the PID loop
+        m_pid_controller = m_left.getPIDController();
+
+        // Set all PID Constants
+
+        // Start the set point to zero
+        set_point = 0;
     }
 
-    /** Drive a precise distance using PID control
-     * @param distance is the distance in feet the robot will travel
-     */
-    public void driveDistance(double distance) {
+    /** Set the distance to the next point and reset motor encoders
+     * @param distance in feet
+    */
+    public void setSetPoint(double distance) {
+        set_point = distance;
+    }
+
+    /** Iterative function that adjusts power to get the drive train to its set point*/
+    public void positionControl() {
         
     }
+
+    
 }
