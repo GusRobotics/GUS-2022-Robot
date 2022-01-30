44cc1ecf3b48092075868f8c718433185ebbc49a
Brennan Lagasse <51427169+BrennanLagasse@users.noreply.github.com>
2022-01-30 13:12:33 -0500

    working on general rule

--- src/main/java/frc/robot/PrecisionDrive.java
+++ src/main/java/frc/robot/PrecisionDrive.java
@@ -4,26 +4,30 @@ package frc.robot;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
-// Neo Resources
+// Rev Resources
 import com.revrobotics.CANSparkMax;
+
+// Cross the Road Resources
+import com.ctre.phoenix.sensors.PigeonIMU;
 
 public class PrecisionDrive {
     private CANSparkMax m_drive_left;
     private CANSparkMax m_drive_right;
-    private double set_point_rev;
+    private double set_point;
     private double rev_to_dist;
+    private PigeonIMU gyro;
 
-    // PID constants
-    public double kP = 0.5;
+    // Tunable PID constants
+    public double kP = 0;
     public double kI = 0;
     public double kD = 0;
     public double kIz = 0;
     public double kFF = 0;
-    public double kMinOutput = -1;
-    public double kMaxOutput = 1;
-
-    // Allowed error in feet
     public double allowedError = 0.025;
+
+    // Constant PID constants
+    public final double kMinOutput = -1;
+    public final double kMaxOutput = 1;
     public double correctTime = 0.125;
 
     // Global storage
@@ -33,57 +37,79 @@ public class PrecisionDrive {
     double first_correct;
     boolean correct;
 
-    public PrecisionDrive(CANSparkMax m_left, CANSparkMax m_right, double conversion) {
-        // Store reference to motors
+    public PrecisionDrive(CANSparkMax m_left, CANSparkMax m_right, double conversion, PigeonIMU pigeon) {
         m_drive_left = m_left;
         m_drive_right = m_right;
-
-        // Set conversion factor
         rev_to_dist = conversion;
+        gyro = pigeon;
 
         // Default instance variables
         last_time = Timer.getFPGATimestamp();
         integral = 0;
     }
 
-    /** Reset PID with new set point. Current position is zeroed
+    /** Reset PID with new set point and set to go a given distance
      * @param distance in feet
     */
-    public void setDistance(double distance) {
-        // Find distance to travel in rev: [dist]/([dist]/[rev])
-        double revs = distance/rev_to_dist;
-
+    public void setDistanceControl(double distance) {
         // This effectively adds the distance in the absence of a proper encoder reset function
-        set_point_rev = m_drive_left.getEncoder().getPosition() + revs;
+        set_point = m_drive_left.getEncoder().getPosition()*rev_to_dist + distance;
 
         // Reset all variables
         last_time = Timer.getFPGATimestamp();
         integral = 0;
-        last_error = revs;
+        last_error = distance;
 
+        // Distance Constants:
+        kP = 0.5;
+        kI = 0;
+        kD = 0;
+        kIz = 0;
+        kFF = 0;
+        allowedError = 0.125;
+    }
+
+    /** Reset PID with new set point and set to rotate a given distance
+     * @param degrees in degrees. This must be between -180 and 180
+    */
+    public void setRotationControl(double degrees) { // NOT DONE
+        // This effectively adds the distance in the absence of a proper encoder reset function
+        // set_point = m_drive_left.getEncoder().getPosition() + revs;
+
+        // Reset all variables
+        last_time = Timer.getFPGATimestamp();
+        integral = 0;
+        last_error = degrees;
+
+        // Distance Constants:
+        kP = 0.1;
+        kI = 0;
+        kD = 0;
+        kIz = 0;
+        kFF = 0;
+        allowedError = 1;
     }
 
     /** Iterative function that adjusts power to get the drive train to its set point
+     * @param val is the current value of the system 
+     * Distance (feet): m_drive_left.getEncoder().getPosition()*rev_to_dist (rotations --> distance)
+     * Rotations (degrees): gyro value (no transformation)
      * @return boolean complete that indicates if the error is within an acceptable range
     */
-    public boolean positionControl() {
-        double error = set_point_rev - m_drive_left.getEncoder().getPosition();
-
-        // Find time elapsed
+    public boolean pidControl(double val) {
+        double error = set_point - val;
         double dt = Timer.getFPGATimestamp() - last_time;
 
-        // Update integral
+        // Integral (potentially cap if too large or reset on passing set point)
         integral += error * dt;
 
-        // Potentially reset integral on passing set point, also consider capping it or reseting it if it gets too big
-
-        // Update derivative
+        // Derivative
         double derivative = (error - last_error)/dt;
 
-        // Determine output speed
+        // Output
         double outputSpeed = error * kP + integral * kI + derivative * kD;
 
-        // Set motors to calculated speed
+        // Set motors to output
         m_drive_left.set(outputSpeed);
         m_drive_right.set(outputSpeed);
 
@@ -92,14 +118,14 @@ public class PrecisionDrive {
         last_error = error;
         
         // Wait a set time
-        // [DELAY]
+        Timer.delay(0.005);
 
         // Print data to shuffleboard (graphing would be great)
-        SmartDashboard.putNumber("encoder value", m_drive_left.getEncoder().getPosition());
+        SmartDashboard.putNumber("value", val);
         SmartDashboard.putNumber("power", outputSpeed);
         SmartDashboard.putNumber("error", error);
 
-        if(Math.abs(error) < allowedError/rev_to_dist) {
+        if(Math.abs(error) < allowedError) {
             if(correct) {
                 // Wait until stable
                 if(Timer.getFPGATimestamp() - first_correct >= correctTime) {
